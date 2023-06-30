56724183785543775ee57aba6a1ccaeecfd7cee4
anthony <anthonyarimalala@gmail.com>
2023-06-30 23:39:40 +0300

    feat: sprint5

--- framework/Fonctions.java
+++ framework/Fonctions.java
@@ -1,24 +1,64 @@
 package etu2033.framework.utils;
 
 import etu2033.framework.Mapping;
-import java.util.*;
+import etu2033.framework.servlet.ModelView;
+import etu2033.annotation.url;
+
 import java.net.URL;
 
 import java.io.*;
-import java.lang.reflect.Method;
+import java.util.*;
+import java.lang.reflect.*;
 
-import etu2033.annotation.url;
 import jakarta.servlet.*;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 public class Fonctions{
 
+    // SPRINT 5
+    // recuperation valeur de retour et dispatcher
+    public static ModelView recup_ModelView(HashMap<String, Mapping> mappingUrls,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out =response.getWriter();
+        
+        ModelView retour = new ModelView();
+        // sprint5
+        String stringUri = request.getRequestURI();
+        String[] arrayPath = stringUri.split("/");
+        String cle = arrayPath[arrayPath.length - 1];
+
+        try{
+            Mapping mapping = new Mapping();
+            for(String key : mappingUrls.keySet()){
+                if(key.equals(cle)){
+                    mapping = mappingUrls.get(key);
+                    break;
+                }
+            }
+            if(!mapping.getClassName().equals("default")){
+                // out.println("tsy null tsony ilay mapping");
+                String nomMethode = cle;
+                String nomDeClasse = (String) mapping.getClassName();
+                java.lang.Class cl = java.lang.Class.forName(nomDeClasse);
+                Object object = cl.newInstance();
+                String method = (String) mapping.getMethod();
+                Method methode = object.getClass().getDeclaredMethod(method);
+                retour = (ModelView) methode.invoke(object);
+                
+
+            }else{
+                // out.println("Null ilay mapping ehh");
+            }
+
+        } catch(Exception e){
+            e.printStackTrace(out);
+        }
+        return retour;
+    }
+
     // SPRINT 3
-    public static void mameno_HashMap(HashMap<String,Mapping> mappingUrls, String packageName) throws Exception{
+    public static HashMap<String,Mapping> mameno_HashMap(HashMap<String,Mapping> mappingUrls, String packageName) throws Exception{
         try {
         mappingUrls = new HashMap<String, Mapping>();
         URL root = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "//")); 
@@ -37,5 +77,6 @@ public class Fonctions{
         catch (Exception e) {
             throw new ServletException(e);
         }
+        return mappingUrls;
     }
 }
--- framework/Mapping.java
+++ framework/Mapping.java
@@ -1,12 +1,22 @@
 package etu2033.framework;
 
 public class Mapping{
-    String className;
-    String method;
+    String className = "default";
+    String method = "default";
 
     public Mapping(String className, String method) {
         this.className = className;
         this.method = method;
+    }
+    public Mapping() {
+    }
+
+    public String getMethod(){
+        return this.method;
+    }
+
+    public String getClassName(){
+        return this.className;
     }
 
 }
